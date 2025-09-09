766abed77c24352195f2bd7ee5803ff9480dc854
MrClouDD <sandhuajitesh@gmail.com>
2025-09-09 17:57:58 -0400

    9.9

---
+++ java/src/main/java/ITEC3150/HW0904.java
@@ -0,0 +1,28 @@
+package ITEC3150;
+
+/**
+ * @author Ajitesh Sandhu
+ * @date 9.8.25
+ *       Simple program to count the number of leaves of a BST
+ *       Depth first search with backtracking
+ */
+
+public class HW0904 {
+    static int getNumberOfLeaves(TreeNode root) {
+        int leafCount = 0;
+
+        TreeNode parent = root;
+        TreeNode current = parent;
+
+        if (current == null) {
+            return 0;
+        }
+
+        if (current.left == null && current.right == null) {
+            return 1;
+
+        }
+
+        return getNumberOfLeaves(current.left) + getNumberOfLeaves(current.right);
+    }
+}
