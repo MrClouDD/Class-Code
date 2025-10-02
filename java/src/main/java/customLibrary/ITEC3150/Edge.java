07f76ff63d74815f9fd8120ad790a4e65ac4f6cf
Ajitesh Sandhu <sandhuajitesh@gmail.com>
2025-10-02 02:39:44 -0400

    Graph

---
+++ java/src/main/java/customLibrary/ITEC3150/Edge.java
@@ -0,0 +1,16 @@
+package customLibrary.ITEC3150;
+
+public class Edge<E> {
+    int u;
+    int v;
+
+    public Edge(int u, int v) {
+        this.u = u;
+        this.v = v;
+    }
+
+    @Override // Test if two edges are identical
+    public boolean equals(Object o) {
+        return u == ((Edge) o).u && v == ((Edge) o).v;
+    }
+}
